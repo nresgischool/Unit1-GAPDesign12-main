6d956783933b351035e106f10d8d902ec65eb8c7
aaaaaaaaaaaaa <74013790+nresgischool@users.noreply.github.com>
2022-02-15 23:02:38 -0800

    almost done

--- Unit 1 Rube Goldberg/Assets/Scripts/CameraManager.cs
+++ Unit 1 Rube Goldberg/Assets/Scripts/CameraManager.cs
@@ -2,30 +2,118 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Camera followCamera;
-    public Camera freeLookCamera;
+    public Camera camera1;
+    public Camera camera2;
+    public Camera camera3;
+    public Camera camera4;
+    public Camera camera5;
+    public Camera camera6;
+    public Camera camera7;
+    public Camera camera8;
 
 
     public void Start()
     {
-        followCamera.enabled = true;
-        freeLookCamera.enabled = false;
+        camera1.enabled = true;
+        camera2.enabled = false;
+        camera3.enabled = false;
+        camera4.enabled = false;
+        camera5.enabled = false;
+        camera6.enabled = false;
+        camera7.enabled = false;
+        camera8.enabled = false;
     }
 
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        //Cycles through cameras when assigned number key is pressed
+        if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if(followCamera.isActiveAndEnabled == true)
-            {
-                followCamera.enabled = false;
-                freeLookCamera.enabled = true;
-            }
-            else
-            {
-                followCamera.enabled = true;
-                freeLookCamera.enabled = false;
-            }
+            camera1.enabled = true;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = false;
+            camera5.enabled = false;
+            camera6.enabled = false;
+            camera7.enabled = false;
+            camera8.enabled = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            camera1.enabled = false;
+            camera2.enabled = true; 
+            camera3.enabled = false;
+            camera4.enabled = false;
+            camera5.enabled = false;
+            camera6.enabled = false;
+            camera7.enabled = false;
+            camera8.enabled = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = true;
+            camera4.enabled = false;
+            camera5.enabled = false;
+            camera6.enabled = false;
+            camera7.enabled = false;
+            camera8.enabled = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = true;
+            camera5.enabled = false;
+            camera6.enabled = false;
+            camera7.enabled = false;
+            camera8.enabled = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = false;
+            camera5.enabled = true;
+            camera6.enabled = false;
+            camera7.enabled = false;
+            camera8.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = false;
+            camera5.enabled = false;
+            camera6.enabled = true;
+            camera7.enabled = false;
+            camera8.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = false;
+            camera5.enabled = false;
+            camera6.enabled = false;
+            camera7.enabled = true;
+            camera8.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = false;
+            camera5.enabled = false;
+            camera6.enabled = false;
+            camera7.enabled = false;
+            camera8.enabled = true;
         }
     }
 }
